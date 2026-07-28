f715159bcdfa6f8220098a590fca9326b59427b4
achoroj3 <achoroj2@gmail.com>
2026-07-28 11:46:41 -0400

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #not quite sure what to do here
+        # initial thought was define a priority queue with a custom comparator
+        #then i realize you cant modify existing elements in the priority queue
+        #so that simply wouldn't work.
+        #ok i have an idea after writing that out
+        #create a hashmap containing the number and frequency of the number
+        #then iterate through the map, and manage a priority queue of the top k numbers
+        #after that, return the priority queues numbers.
+        
+        frequency_table = {} # number:frequency
+        for elem in nums:
+            if elem not in frequency_table:
+                frequency_table[elem] = 1
+            else:
+                frequency_table[elem]+= 1
+        pq = []
+        heapq.heapify_max(pq)
+        for key,value in frequency_table.items():
+            if len(pq) < k:
+                heapq.heappush_max(pq, (value, key)) # value is the priority
+            else:
+                pq_temp = pq.copy()
+                pq = []
+                heapq.heapify_max(pq)
+                while (len(pq_temp) > 1):
+                    heapq.heappush_max(pq, heapq.heappop_max(pq_temp))
+                if pq_temp[0][0] < value:
+                    heapq.heappush_max(pq, (value, key))
+                else:
+                    heapq.heappush_max(pq, heapq.heappop_max(pq_temp))
+
+        answer=[]
+        while (len(pq) > 0):
+            answer.append(heapq.heappop_max(pq)[1])
+        return answer
+            
+
+
+
+
