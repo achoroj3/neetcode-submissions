e40c8cc4a7dd612a4afb990aa3e3cd6aa5e051d2
achoroj3 <achoroj2@gmail.com>
2026-08-18 13:39:32 -0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.lowest = None
+        self.reps = {}
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        cur = len(self.stack) - 1
+        if self.reps == {}:
+            self.lowest = 0
+            self.reps[cur] = 0
+        else:
+            if val >= self.stack[self.lowest]:
+                self.reps[cur] = self.lowest
+            else:
+                self.lowest = cur
+                self.reps[cur] = cur
+    def pop(self) -> None:
+        del self.stack[-1]
+        cur = len(self.stack) - 1
+        if cur == -1:
+            self.lowest = 0
+        else:
+            self.lowest = self.reps[len(self.stack) - 1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.stack[self.lowest]
+        
